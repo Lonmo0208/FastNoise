8a266267020a38ac6dee8940b309f84e7018b0e7
ZenXArch <zenxarch@noreply.codeberg.org>
2025-12-13 08:36:26 +0000

    remove seed param

--- src/jmh/java/org/codeberg/zenxarch/fastnoise/noise/ArrayState.java
+++ src/jmh/java/org/codeberg/zenxarch/fastnoise/noise/ArrayState.java
@@ -3,7 +3,6 @@ package org.codeberg.zenxarch.fastnoise.noise;
 import java.util.Random;
 import net.minecraft.util.collection.PackedIntegerArray;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -14,8 +13,7 @@ public class ArrayState {
   long[] big = new long[128];
   PackedIntegerArray packedArray = new PackedIntegerArray(2, 16 * 16 * 16, big);
 
-  @Param(value = {"0", "1", "2", "3", "4", "5"})
-  int seed;
+  int seed = 63;
 
   Random random = new Random(this.seed);
 
