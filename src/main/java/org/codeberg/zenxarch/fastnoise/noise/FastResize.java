f067de75b5d973dc4eebcfaf4d66b38aeab211bc
ZenXArch <zenxarch@noreply.codeberg.org>
2025-12-25 12:13:02 +0000

    4x faster expand

--- src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastResize.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastResize.java
@@ -1,15 +1,19 @@
 package org.codeberg.zenxarch.fastnoise.noise;
 
 public final class FastResize {
+  private static long expand(long in) {
+    long result = 0x0l;
+    for (int i = 0; i < 32; i++) {
+      result |= (in & 0x1) << (i << 1);
+      in = in >>> 1;
+    }
+    return result;
+  }
 
-  // fast expand bits using mask
   public static void fastResize1to2bits(long[] small, long[] large) {
-    // is 0b010101....
-    long mask = 0x5555_5555_5555_5555L;
-
     for (int i = 0; i < small.length; i++) {
-      large[(i << 1)] = Long.expand(small[i], mask);
-      large[(i << 1) | 1] = Long.expand(small[i] >> 32, mask);
+      large[i + i] = expand(small[i]);
+      large[i + i + 1] = expand(small[i] >>> 32);
     }
   }
 }
