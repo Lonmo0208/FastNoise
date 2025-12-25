d5a2e4d5cc355e3c22a234826080cbec99fd11a6
ZenXArch <zenxarch@noreply.codeberg.org>
2025-12-25 15:29:47 +0000

    2x improvement

--- src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastResize.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastResize.java
@@ -1,16 +1,24 @@
 package org.codeberg.zenxarch.fastnoise.noise;
 
 public final class FastResize {
-  private static long expand2bit(long in) {
-    return in + (in & 0x2);
+  private static long expand16bit(long in) {
+    var a =
+        ((in & 0b0000_0101_0000_0101l) * 0b0000_0101_0000_0101l)
+            & Long.expand(0b0000_0101_0000_0101l, 0x5555555555555555l);
+    var b =
+        ((in & 0b0000_1010_0000_1010l) * 0b0000_1010_0000_1010l)
+            & Long.expand(0b0000_1010_0000_1010l, 0x5555555555555555l);
+    var c =
+        ((in & 0b0101_0000_0101_0000l) * 0b0101_0000_0101_0000l)
+            & Long.expand(0b0101_0000_0101_0000l, 0x5555555555555555l);
+    var d =
+        ((in & 0b1010_0000_1010_0000l) * 0b1010_0000_1010_0000l)
+            & Long.expand(0b1010_0000_1010_0000l, 0x5555555555555555l);
+    return a | b | c | d;
   }
 
   private static long expand(long in) {
-    long result = 0x0l;
-    for (int i = 0; i < 16; i++) {
-      result |= expand2bit((in >>> i >>> i) & 0x3) << (4 * i);
-    }
-    return result;
+    return expand16bit(in & 0xFFFF) | (expand16bit(in >>> 16) << 32);
   }
 
   public static void fastResize1to2bits(long[] small, long[] large) {
