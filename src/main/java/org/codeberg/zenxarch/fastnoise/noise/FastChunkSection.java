ec836799b09d46128556e90b8bb1a41cba134fe3
ZenXArch <zenxarch@noreply.codeberg.org>
2025-12-28 05:36:23 +0000

    resize skip

--- src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastChunkSection.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/noise/FastChunkSection.java
@@ -10,6 +10,7 @@ import org.codeberg.zenxarch.fastnoise.mixin.PalettedContainerAccessor;
 public final class FastChunkSection implements PaletteResizeListener<BlockState> {
 
   private final ChunkSection section;
+  private int maxIdx = Integer.MIN_VALUE;
 
   public FastChunkSection(ChunkSection section) {
     this.section = section;
@@ -17,6 +18,7 @@ public final class FastChunkSection implements PaletteResizeListener<BlockState>
 
   public void setBlockState(int x, int y, int z, BlockState state) {
     var blkidx = (((y << 4) | z) << 4) | x;
+    maxIdx = Math.max(maxIdx, blkidx);
     var valIdx = section.blockStateContainer.data.palette().index(state, this);
 
     section.blockStateContainer.data.storage().zenxarch$unsafeSet(blkidx, valIdx);
@@ -37,7 +39,7 @@ public final class FastChunkSection implements PaletteResizeListener<BlockState>
         canUseFastImport = false;
     }
 
-    if (canUseFastImport) fastImport(oldData, newData, newBits);
+    if (canUseFastImport) fastImport(oldData, newData, newBits, this.maxIdx);
     else newData.importFrom(oldData.palette(), oldData.storage());
 
     section.blockStateContainer.data = newData;
@@ -45,19 +47,20 @@ public final class FastChunkSection implements PaletteResizeListener<BlockState>
     return newData.palette().index(object, PaletteResizeListener.throwing());
   }
 
-  private static <T> void fastImport(Data<T> oldData, Data<T> newData, int newBits) {
+  private static <T> void fastImport(Data<T> oldData, Data<T> newData, int newBits, int upto) {
     var oldStorage = oldData.storage();
     var newStorage = newData.storage();
     var oldStorageData = oldStorage.getData();
     var newStorageData = newStorage.getData();
 
     if (newBits == 2) FastResize.fastResize1to2bits(oldStorageData, newStorageData);
-    else fastImport(oldStorage, newStorage);
+    else fastImport(oldStorage, newStorage, upto);
   }
 
-  private static <T> void fastImport(PaletteStorage oldStorage, PaletteStorage newStorage) {
-    for (int i = 0; i < oldStorage.getSize(); i++)
-      newStorage.zenxarch$unsafeSet(i, oldStorage.get(i));
+  private static <T> void fastImport(
+      PaletteStorage oldStorage, PaletteStorage newStorage, int upto) {
+    var size = Math.min(upto, oldStorage.getSize());
+    for (int i = 0; i < size; i++) newStorage.zenxarch$unsafeSet(i, oldStorage.get(i));
   }
 
   public void recalculateCounts() {
