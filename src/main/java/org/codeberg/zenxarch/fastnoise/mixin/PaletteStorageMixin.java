b09a44bb588a368d6d0700ccea2107bf0471dc2a
ZenXArch <zenxarch@noreply.codeberg.org>
2026-01-04 14:25:13 +0000

    fix possible incompat with mods implement their own palette storage

---
+++ src/main/java/org/codeberg/zenxarch/fastnoise/mixin/PaletteStorageMixin.java
@@ -0,0 +1,17 @@
+package org.codeberg.zenxarch.fastnoise.mixin;
+
+import net.minecraft.util.collection.PaletteStorage;
+import org.codeberg.zenxarch.fastnoise.noise.FastPackedIntegerArray;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(PaletteStorage.class)
+public interface PaletteStorageMixin extends FastPackedIntegerArray {
+  @Shadow
+  void set(int index, int value);
+
+  @Override
+  default void zenxarch$unsafeSet(int index, int value) {
+    set(index, value);
+  }
+}
