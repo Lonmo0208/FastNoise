e800f96ca9c2e111c13854c44e13f9e1cfa6e6c1
ZenXArch <zenxarch@noreply.codeberg.org>
2026-01-05 12:02:59 +0000

    Add another benchmark
    Results:
    ```
    Benchmark                          Mode  Cnt        Score      Error  Units
    FastResizeBenchmark.optimized     thrpt    8  2142409.646 ± 3470.780  ops/s
    FastResizeBenchmark.optimizedSet  thrpt    8   553390.340 ±  662.890  ops/s
    FastResizeBenchmark.vanilla       thrpt    8    73021.411 ±   72.909  ops/s
    ```

--- src/jmh/java/org/codeberg/zenxarch/fastnoise/noise/FastResizeBenchmark.java
+++ src/jmh/java/org/codeberg/zenxarch/fastnoise/noise/FastResizeBenchmark.java
@@ -18,4 +18,25 @@ public class FastResizeBenchmark {
     FastResize.fastResize1to2bits(state.small, state.big);
     hole.consume(state);
   }
+
+  public static void fastResize1to2bits(long[] small, long[] large) {
+    
+  }
+
+  private long expand(long ix) {
+    long result = 0x0L;
+    for (int i = 0; i < 32;i++) {
+      result |= ((ix >> i) & 0x1L) << (i << 1);
+    }
+    return result;
+  }
+
+  @Benchmark
+  public void optimizedSet(ArrayState state, Blackhole hole) {
+    for (int i = 0; i < state.small.length; i++) {
+      state.big[i + i] = expand(state.small[i]);
+      state.big[i + i + 1] = expand(state.small[i] >>> 32);
+    }
+    hole.consume(state);
+  }
 }
