d3ecb5bd220e5869c9014f1aae7cb6312ab33a13
ZenXArch <zenxarch@noreply.codeberg.org>
2026-01-20 09:43:34 +0000

    fix shadow

--- src/main/java/org/codeberg/zenxarch/fastnoise/mixin/EntriesMixin.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/mixin/EntriesMixin.java
@@ -10,6 +10,7 @@ import org.codeberg.zenxarch.fastnoise.FastNoiseMod;
 import org.codeberg.zenxarch.fastnoise.tree.FastSearchTree;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entries.class)
 public abstract class EntriesMixin<T> {
-  private @Final SearchTree<T> tree;
+  @Shadow @Final private SearchTree<T> tree;
 
   @Unique @Final private FastSearchTree<T> zenxarch$searchtree;
 
