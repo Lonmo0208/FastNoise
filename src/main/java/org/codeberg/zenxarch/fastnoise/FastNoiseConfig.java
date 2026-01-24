25e91f80caf22b8f5546ef6a02a4f3aaab0a2797
ZenXArch <zenxarch@noreply.codeberg.org>
2026-01-24 08:46:55 +0000

    allow mods to override mixins via mod metadata

--- src/main/java/org/codeberg/zenxarch/fastnoise/FastNoiseConfig.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/FastNoiseConfig.java
@@ -7,10 +7,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.CustomValue.CvArray;
+import net.fabricmc.loader.api.metadata.CustomValue.CvObject;
+import net.fabricmc.loader.api.metadata.CustomValue.CvType;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.util.Util;
 
 public class FastNoiseConfig {
   private static final String configFileName = FastNoiseConstants.MOD_ID + ".mixin.properties";
+
+  private static final String overridesKey = FastNoiseConstants.MOD_ID + ":overrides";
 
   private static final String[] keys = new String[] {"mixin.perf.noise", "mixin.perf.biome"};
 
@@ -72,6 +78,59 @@ public class FastNoiseConfig {
     }
   }
 
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModMetadata meta, String key, boolean value) {
+    if (!map.containsKey(key)) {
+      FastNoiseConstants.LOGGER.error("Mod {} tried to override unknown key {}", meta.getId(), key);
+    } else {
+      FastNoiseConstants.LOGGER.info(
+          "Mod {} override key {} with value {}", meta.getId(), key, value);
+      map.put(key, value);
+    }
+  }
+
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModMetadata meta, CvArray array) {
+    for (var value : array) {
+      if (value.getType() != CvType.STRING) {
+        FastNoiseConstants.LOGGER.error(
+            "Mod {} has array of not strings as overrides", meta.getId());
+        continue;
+      }
+      collectOverrides(map, meta, value.getAsString(), false);
+    }
+  }
+
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModMetadata meta, CvObject object) {
+    for (var value : object) {
+      if (value.getValue().getType() != CvType.BOOLEAN) {
+        FastNoiseConstants.LOGGER.error(
+            "Mod {} has object of not booleans as overrides", meta.getId());
+        continue;
+      }
+      collectOverrides(map, meta, value.getKey(), value.getValue().getAsBoolean());
+    }
+  }
+
+  private static void collectOverrides(Object2BooleanArrayMap<String> map) {
+    for (var container : FabricLoader.getInstance().getAllMods()) {
+      var meta = container.getMetadata();
+      if (!meta.containsCustomValue(overridesKey)) continue;
+
+      var value = meta.getCustomValue(overridesKey);
+      switch (value.getType()) {
+        case CvType.OBJECT -> collectOverrides(map, meta, value.getAsObject());
+        case CvType.ARRAY -> collectOverrides(map, meta, value.getAsArray());
+        case CvType.STRING -> collectOverrides(map, meta, value.getAsString(), false);
+        default ->
+            FastNoiseConstants.LOGGER.error(
+                "Mod {} has unsupported overrides of type {}", meta.getId(), value.getType());
+      }
+      ;
+    }
+  }
+
   public static Object2BooleanMap<String> loadConfig() {
     var props = loadProperties();
     var result = new Object2BooleanArrayMap<String>();
@@ -87,6 +146,7 @@ public class FastNoiseConfig {
     }
 
     saveProperties(props);
-    return result;
+    collectOverrides(result);
+    return Object2BooleanMaps.unmodifiable(result);
   }
 }
