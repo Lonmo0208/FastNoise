734702d345f41f133d4d8ec12879188116204f31
ZenXArch <zenxarch@noreply.codeberg.org>
2026-02-14 07:06:50 +0000

    we can copy stuff now

--- perf/src/main/java/org/codeberg/zenxarch/fastnoise/ChunkRegion.java
+++ perf/src/main/java/org/codeberg/zenxarch/fastnoise/ChunkRegion.java
@@ -7,6 +7,7 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.ProtoChunk;
 
 public record ChunkRegion(ProtoChunk[] chunks, Object2IntMap<ChunkPos> chunkGetter)
@@ -32,5 +33,28 @@ public record ChunkRegion(ProtoChunk[] chunks, Object2IntMap<ChunkPos> chunkGett
   public RegistryEntry<Biome> getBiomeForNoiseGen(int biomeX, int biomeY, int biomeZ) {
     var chunk = chunks[chunkGetter.applyAsInt(new ChunkPos(biomeX >> 2, biomeZ >>> 2))];
     return chunk.getBiomeForNoiseGen(biomeX, biomeY, biomeZ);
+  }
+
+  public void copyNoiseAndHeightmap(ChunkRegion region) {
+    for (int i = 0; i < this.chunks.length; i++) {
+      var dest = this.chunks[i];
+      var src = region.getChunk(dest.getPos());
+
+      var srcData = src.getSectionArray();
+      var destData = dest.getSectionArray();
+
+      for (int j = 0; j < srcData.length; j++) {
+        destData[j] =
+            new ChunkSection(srcData[j].blockStateContainer.copy(), destData[j].biomeContainer);
+      }
+
+      for (var heightmap : src.getHeightmaps()) {
+        dest.setHeightmap(heightmap.getKey(), heightmap.getValue().asLongArray());
+      }
+    }
+  }
+
+  public ProtoChunk getChunk(ChunkPos pos) {
+    return this.chunks[this.chunkGetter.getInt(pos)];
   }
 }
