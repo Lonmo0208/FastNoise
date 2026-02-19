8771f3a58891c833ee062e92fb9b3b42f8b07248
ZenXArch <zenxarch@noreply.codeberg.org>
2026-02-19 14:11:45 +0000

    faster to do this

--- src/main/java/org/codeberg/zenxarch/fastnoise/surface/MaterialRuleContext.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/surface/MaterialRuleContext.java
@@ -53,6 +53,11 @@ public class MaterialRuleContext extends MaterialRules.MaterialRuleContext {
       int blockZ) {
     super.initVerticalContext(
         stoneDepthAbove, stoneDepthBelow, fluidHeight, blockX, blockY, blockZ);
+    
+    var x = blockX & 15;
+    if (x < 2 || x > 14) return;
+    var z = blockZ & 15;
+    if (z < 2 || z > 14) return;
 
     var y = blockY - minY;
     var ly = y & 0x15;
@@ -71,11 +76,6 @@ public class MaterialRuleContext extends MaterialRules.MaterialRuleContext {
       if (cy == (this.singleBiomes.length - 1)) return;
       if (singleBiomes[cy] != singleBiomes[cy + 1]) return;
     }
-
-    var x = blockX & 15;
-    if (x < 2 || x > 14) return;
-    var z = blockZ & 15;
-    if (z < 2 || z > 14) return;
     
     this.biomeSupplier = () -> single;
   }
