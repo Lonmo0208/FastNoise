f2bbddd4673e17f83d54f63d115a22e38ed3a484
ZenXArch <zenxarch@noreply.codeberg.org>
2026-02-21 07:05:53 +0000

    new system, new config entries

---
+++ src/main/java/org/codeberg/zenxarch/fastnoise/config/BooleanConfigEntry.java
@@ -0,0 +1,15 @@
+package org.codeberg.zenxarch.fastnoise.config;
+
+public record BooleanConfigEntry(String key, String comment, boolean defaultValue) {
+  public static BooleanConfigEntry of(String key, String comment, boolean defaultValue) {
+    return new BooleanConfigEntry(key, comment, defaultValue);
+  }
+
+  public static BooleanConfigEntry mixin(String key, String comment, boolean defaultValue) {
+    return new BooleanConfigEntry("mixin." + key, comment, defaultValue);
+  }
+
+  public boolean isMixin() {
+    return this.key.startsWith("mixin.");
+  }
+}
