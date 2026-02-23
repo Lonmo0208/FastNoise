ce413fcd5e32af64462636b6c1c97b5097668e16
ZenXArch <zenxarch@noreply.codeberg.org>
2026-02-23 13:11:07 +0000

    fix override loading

--- src/main/java/org/codeberg/zenxarch/fastnoise/config/FastNoiseConfig.java
+++ src/main/java/org/codeberg/zenxarch/fastnoise/config/FastNoiseConfig.java
@@ -3,15 +3,13 @@ package org.codeberg.zenxarch.fastnoise.config;
 import it.unimi.dsi.fastutil.objects.Object2BooleanArrayMap;
 import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
 import it.unimi.dsi.fastutil.objects.Object2BooleanMaps;
-// import net.fabricmc.loader.api.FabricLoader;
-// import net.fabricmc.loader.api.metadata.CustomValue.CvArray;
-// import net.fabricmc.loader.api.metadata.CustomValue.CvObject;
-// import net.fabricmc.loader.api.metadata.CustomValue.CvType;
-// import net.fabricmc.loader.api.metadata.ModMetadata;
+import java.util.List;
+import java.util.Map;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.moddiscovery.ModInfo;
 import org.codeberg.zenxarch.fastnoise.FastNoiseConstants;
 
 public class FastNoiseConfig {
-  // TODO: Fix overrides
   private static final String overridesKey = FastNoiseConstants.MOD_ID + ":overrides";
 
   static boolean get(BooleanConfigEntry entry) {
@@ -26,58 +24,61 @@ public class FastNoiseConfig {
   public static final boolean OPTIMIZE_BIOME_ACCESS =
       get(FastNoiseConfigEntries.OPTIMIZE_BIOME_ACCESS);
 
-  // private static void collectOverrides(
-  //     Object2BooleanArrayMap<String> map, ModMetadata meta, String key, boolean value) {
-  //   if (!map.containsKey(key)) {
-  //     FastNoiseConstants.LOGGER.error("Mod {} tried to override unknown key {}", meta.getId(),
-  // key);
-  //   } else {
-  //     FastNoiseConstants.LOGGER.info(
-  //         "Mod {} override key {} with value {}", meta.getId(), key, value);
-  //     map.put(key, value);
-  //   }
-  // }
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModInfo meta, String key, boolean value) {
+    if (!map.containsKey(key)) {
+      FastNoiseConstants.LOGGER.error(
+          "Mod {} tried to override unknown key {}", meta.getModId(), key);
+    } else {
+      FastNoiseConstants.LOGGER.info(
+          "Mod {} override key {} with value {}", meta.getModId(), key, value);
+      map.put(key, value);
+    }
+  }
 
-  // private static void collectOverrides(
-  //     Object2BooleanArrayMap<String> map, ModMetadata meta, CvArray array) {
-  //   for (var value : array) {
-  //     if (value.getType() != CvType.STRING) {
-  //       FastNoiseConstants.LOGGER.error(
-  //           "Mod {} has array of not strings as overrides", meta.getId());
-  //       continue;
-  //     }
-  //     collectOverrides(map, meta, value.getAsString(), false);
-  //   }
-  // }
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModInfo meta, List<?> array) {
+    for (var value : array) {
+      if (!(value instanceof String string)) {
+        FastNoiseConstants.LOGGER.error(
+            "Mod {} has array of not strings as overrides", meta.getModId());
+        continue;
+      }
+      collectOverrides(map, meta, string, false);
+    }
+  }
 
-  // private static void collectOverrides(
-  //     Object2BooleanArrayMap<String> map, ModMetadata meta, CvObject object) {
-  //   for (var value : object) {
-  //     if (value.getValue().getType() != CvType.BOOLEAN) {
-  //       FastNoiseConstants.LOGGER.error(
-  //           "Mod {} has object of not booleans as overrides", meta.getId());
-  //       continue;
-  //     }
-  //     collectOverrides(map, meta, value.getKey(), value.getValue().getAsBoolean());
-  //   }
-  // }
+  private static void collectOverrides(
+      Object2BooleanArrayMap<String> map, ModInfo meta, Map<?, ?> object) {
+    for (var value : object.keySet()) {
+      if (!(value instanceof String key)) continue;
+      if (!(object.get(value) instanceof Boolean bl)) {
+        FastNoiseConstants.LOGGER.error(
+            "Mod {} has object of not booleans as overrides", meta.getModId());
+        continue;
+      }
+      collectOverrides(map, meta, key, bl);
+    }
+  }
 
-  // private static void collectOverrides(Object2BooleanArrayMap<String> map) {
-  //   for (var container : FabricLoader.getInstance().getAllMods()) {
-  //     var meta = container.getMetadata();
-  //     if (!meta.containsCustomValue(overridesKey)) continue;
+  private static void collectOverrides(Object2BooleanArrayMap<String> map) {
+    for (var container : FMLLoader.getCurrent().getLoadingModList().getMods()) {
+      var meta = container.getConfigElement(overridesKey);
+      if (meta.isEmpty()) continue;
 
-  //     var value = meta.getCustomValue(overridesKey);
-  //     switch (value.getType()) {
-  //       case CvType.OBJECT -> collectOverrides(map, meta, value.getAsObject());
-  //       case CvType.ARRAY -> collectOverrides(map, meta, value.getAsArray());
-  //       case CvType.STRING -> collectOverrides(map, meta, value.getAsString(), false);
-  //       default ->
-  //           FastNoiseConstants.LOGGER.error(
-  //               "Mod {} has unsupported overrides of type {}", meta.getId(), value.getType());
-  //     }
-  //   }
-  // }
+      var value = meta.get();
+      switch (value) {
+        case Map<?, ?> otherMap -> collectOverrides(map, container, otherMap);
+        case List<?> list -> collectOverrides(map, container, list);
+        case String string -> collectOverrides(map, container, string, false);
+        default ->
+            FastNoiseConstants.LOGGER.error(
+                "Mod {} has unsupported overrides of type {}",
+                container.getModId(),
+                value.getClass().getSimpleName());
+      }
+    }
+  }
 
   public static Object2BooleanMap<String> loadConfig() {
     var result = new Object2BooleanArrayMap<String>();
@@ -87,7 +88,7 @@ public class FastNoiseConfig {
       result.put(entry.key(), r);
     }
 
-    // collectOverrides(result);
+    collectOverrides(result);
     return Object2BooleanMaps.unmodifiable(result);
   }
 }
