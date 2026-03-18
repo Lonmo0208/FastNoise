bc6b2240d81e2f8827c3a2f9ce3605828a49505a
ZenXArch <zenxarch@noreply.codeberg.org>
2026-03-18 16:10:53 +0000

    full chunk cache

---
+++ src/main/java/org/codeberg/zenxarch/fastnoise/surface/cache/FastChunkCache.java
@@ -0,0 +1,22 @@
+package org.codeberg.zenxarch.fastnoise.surface.cache;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.world.chunk.Chunk;
+
+public final class FastChunkCache {
+  private final FastSectionCache[] caches;
+  private final int minY;
+
+  public FastChunkCache(Chunk chunk, BlockState defaultState) {
+    var sections = chunk.getSectionArray();
+    this.caches = new FastSectionCache[sections.length];
+    for (int i = 0; i < sections.length; i++)
+      caches[i] = new FastSectionCache(sections[i], defaultState);
+    this.minY = chunk.getBottomY();
+  }
+
+  public FastSectionCache.STATE getState(int x, int y, int z) {
+    var sy = (y - minY) >> 4;
+    return caches[sy].getState(x, y & 0xF, z);
+  }
+}
